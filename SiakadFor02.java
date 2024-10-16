fc579e587d5d8f80c9b677b995449e6cbcae9611
azaria amanda <azaryaamanda@gmail.com>
2024-10-16 13:56:33 +0700

    Modifikasi Percobaan 1

--- SiakadFor02.java
+++ SiakadFor02.java
@@ -8,6 +8,7 @@ public class SiakadFor02 {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -18,8 +19,16 @@ public class SiakadFor02 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+
+            }
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + tidakLulus);
     }
 }
